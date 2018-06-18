9fd94901b596906a938e48efceff272bcb35271e
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-18 16:38:44 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -5,13 +5,19 @@ def square_array(array)
   newarray
 end
 
-using map/collect
-def square_array(array)
+# using map/collect
+
+def square_arrayy(array)
   # your code here
   array.map {|num| num*num }
 end
 
-def square_array(array)
+def square_arrayyy(array)
   # your code here
   array.collect {|num| num*num }
 end
+
+
+# puts square_array([1,2,4])
+# puts square_arrayy([1,2,4])
+# puts square_arrayy([1,2,4])
